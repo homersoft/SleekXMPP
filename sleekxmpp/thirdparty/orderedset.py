e955229b7b14046de6f6f66441a904e1d0f6586b
Rafał Gajda <rafal.gajda@silvair.com>
2024-08-21 17:43:01 +0200

    SP-NONE: Update collections reference to allow use on python3.10

--- sleekxmpp/thirdparty/orderedset.py
+++ sleekxmpp/thirdparty/orderedset.py
@@ -22,7 +22,8 @@
 
 import collections
 
-class OrderedSet(collections.MutableSet):
+
+class OrderedSet(collections.abc.MutableSet):
 
     def __init__(self, iterable=None):
         self.end = end = []
